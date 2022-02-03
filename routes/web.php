319b335cb4f1142b6d589a4c276e2d52f372f1eb
nivaschenko <nivaschenko@aristeksystems.com>
2022-02-03 21:13:54 +0300

    rollbar init

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskStatusController;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
+    Log::debug('Test debug message');
     return view('welcome');
 });
 
